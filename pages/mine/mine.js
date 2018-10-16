4ff230f07ff94bed720524d53b7ea5e966bf384c
kb <kb1949@163.com>
2018-10-16 16:25:53 +0800

    bate

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,11 +1,22 @@
+import util from '../../utils/util.js'
+import cache from '../../utils/cache.js'
+import network from '../../utils/ajax.js'
 // pages/mine/mine.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    login:true,
+    done:false,
+    avatarUrl:'../img/touxiang.png',
+  },
+  getData(){
+    network.get(
+      'sysUser/123456',
+      {id:cache.get('id')}).then((res)=>{
+        if(res.meta.success){
+          if(res.data.workNumber){
+            this.setData({data:res.data,done:true})
+          }
+        }
+      })
   },
   // 获取用户信息
   onUserinfo(e){
@@ -14,9 +25,21 @@ Page({
       this.setData({login:false,userInfo})
     }
   },
-  // 信息设置
-  onSet(e){
-    console.log("安全设置")
+  // 修改手机号
+  onChangeTel(){
+    let data = this.data.data;
+    let isCard = cache.get('IdCard');
+    if(isCard){
+      wx.navigateTo({
+        url: './changePhone/changePhone'
+      })
+    }else{
+      util.showModal('温馨提示','请先绑定身份证号即可修改',true,(()=>{
+        wx.navigateTo({
+          url: './bindIDCard/bindIDCard'
+        })
+      }),(()=>{}),'返回','确定','#ff5800','#ff5800')
+    }
   },
   // 安全退出
   onQit(e){
@@ -26,7 +49,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getData();
   },
 
   /**
